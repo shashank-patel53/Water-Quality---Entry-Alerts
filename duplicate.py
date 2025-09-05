4bead9f8801993406b7e7458a3c88b34cdeba9ca
shashank-patel53 <shashankpatelofficial4455@gmail.com>
2025-09-05 10:32:44 +0530

    commit1

--- duplicate.py
+++ duplicate.py
@@ -409,12 +409,3 @@ def update_thresh():
 if __name__ == "__main__":
     init_db()
     app.run(debug=True, host="0.0.0.0", port=5000)
-                         
-
-
-
-                         
-
-
-
-
